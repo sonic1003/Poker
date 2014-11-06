02d16d7959b6bb413dd413f1b359d4371d051e38
wei shao <moonlight.ranger@gmail.com>
2014-11-05 22:20:35 -0800

    initial submit

---
+++ PokerApplication/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerApplication
+{
+
+    public class Card
+    {
+        private Value _value;
+        private Suit _suit;
+
+        public Card (Value value, Suit suit)
+        {
+            _value = value;
+            _suit = suit;
+        }
+    }
+}
---
+++ PokerApplication/Enums.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerApplication
+{
+    public enum Suit
+    {
+        spade,
+        heart,
+        diamond,
+        club,
+    };
+
+    public enum Value
+    {
+        one,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten,
+        J,
+        Q,
+        K,
+        A,
+    };
+}
