8f8213dea723228b6c7c53bcc92114a94b1b904d
wei shao <moonlight.ranger@gmail.com>
2014-11-06 16:23:31 -0800

    sorting algorithm

--- PokerApplication/PokerBizCore.cs
+++ PokerApplication/PokerBizCore.cs
@@ -8,5 +8,45 @@ namespace PokerApplication
 {
     class PokerBizCore
     {
+
+        public static void Sort(int[] numbers)
+        {
+            Sort(numbers, 0, numbers.Length - 1);
+        }
+
+        private static void Sort(int[] numbers, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = numbers[(left + right) / 2];
+                int i = left - 1;
+                int j = right + 1;
+                while (true)
+                {
+                    while (numbers[++i] < middle)
+                        ;
+
+                    while (numbers[--j] > middle)
+                        ;
+
+                    if (i >= j)
+                        break;
+
+                    Swap(numbers, i, j);
+                }
+
+                Sort(numbers, left, i - 1);
+                Sort(numbers, j + 1, right);
+            }
+        }
+
+        private static void Swap(int[] numbers, int i, int j)
+        {
+            int number = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = number;
+        }
+
+
     }
 }
--- PokerApplication/Program.cs
+++ PokerApplication/Program.cs
@@ -3,14 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace PokerApplication
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            for (int n = 0; n < 100; ++n)
+            {
+                Stopwatch sp = new Stopwatch();
+                Random num = new Random();
 
+                sp.Reset();
+                sp.Start();
+                List<int> list = new List<int>();
+
+                for (int i = 0; i < 100000; ++i)
+                {
+                    list.Add(num.Next());
+                }
+                sp.Stop();
+                Console.WriteLine(sp.ElapsedMilliseconds + " , " + sp.ElapsedTicks);
+
+
+
+                sp.Reset();
+                sp.Start();
+                int[] array = new int[100000];
+                for (int i = 0; i < 100000; ++i)
+                {
+                    array[i] = num.Next();
+                }
+                sp.Stop();
+                Console.WriteLine(sp.ElapsedMilliseconds + " , " + sp.ElapsedTicks);
+
+                sp.Reset();
+                sp.Start();
+                PokerBizCore.Sort(array);
+                sp.Stop();
+                Console.WriteLine(sp.ElapsedMilliseconds + " , " + sp.ElapsedTicks);
+
+                sp.Reset();
+                sp.Start();
+                list.Sort();
+                sp.Stop();
+                Console.WriteLine(sp.ElapsedMilliseconds + " , " + sp.ElapsedTicks);
+
+
+                Console.WriteLine("\n-------------------------\n");
+            }
         }
+            
     }
 }
